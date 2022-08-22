8fef37396beecffb926fc13723bfa3f0a2220a02
io-qar <40149230+io-qar@users.noreply.github.com>
2022-08-22 17:07:24 +0300

    Delete base.sql

--- base.sql
+++
@@ -1,6 +0,0 @@
-
-CREATE DATABASE tg_bot
-
-CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT NOT NULL);
-
-CREATE TABLE requests (id_request SERIAL PRIMARY KEY,request TEXT,id_user INTEGER,FOREIGN KEY (id_user) REFERENCES users(id));
